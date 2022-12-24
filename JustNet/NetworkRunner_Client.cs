05982bee5b97dedded303f6d49c8399b35959614
Starplayer39 <72770312+Starplayer39@users.noreply.github.com>
2022-12-24 13:08:53 +0900

    Sharing unstable work
    Currently working on project and sharing unstable work. It contains a lot of error.

---
+++ JustNet/NetworkRunner_Client.cs
@@ -0,0 +1,278 @@
+﻿/*
+ * JustNet - Just some code for studying part of C# TCP networking
+ * 
+ * Copyright(c) 2022, Starplayer39
+ * The project is under BSD 3-Clause License. Please see the LICENSE.txt
+*/
+
+using System.Net.Sockets;
+using System.Net;
+using static JustNet.NetworkRunner.Constant;
+using static JustNet.PacketPacker;
+
+namespace JustNet
+{
+    public sealed partial class NetworkRunner
+    {
+        public sealed class Client
+        {
+            public uint ReadBufferSize
+            {
+                get => readBufferSize;
+                set
+                {
+                    if (IsClientReady || value <= 0)
+                    {
+                        return;
+                    }
+
+                    readBufferSize = value;
+                }
+            }
+
+            public uint Port
+            {
+                get => port;
+                set
+                {
+                    if (IsClientReady)
+                    {
+                        return;
+                    }
+
+                    port = value;
+                }
+            }
+
+            public uint ClientID { get; private set; }
+
+            public bool IsClientReady { get; private set; }
+
+            public bool IsClientReadable { get; private set; }
+
+            public bool IsClientWritable { get; private set; }
+
+            public bool IsClientRunning { get => IsClientReady && IsClientWritable && IsClientReadable; }
+
+            public uint port;
+            public uint readBufferSize;
+
+            public Action OnClientStart;
+            public Action OnClientStop;
+            public Action OnConnectedToServer;
+            public Action<int, ReadablePacket> OnDataReceivedFromServer;
+
+            private TcpClient tcpClient;
+            private NetworkStream networkStream;
+
+            private byte[] readBuffer;
+
+            public Client()
+            {
+                ClientID = 123456789; // Default
+                readBufferSize = 1024; // Default
+                port = 12345; // Default
+
+                tcpClient = null;
+                networkStream = null;
+
+                IsClientReady = false;
+                IsClientReadable = false;
+                IsClientWritable = false;
+
+                OnClientStart = null;
+                OnClientStop = null;
+                OnConnectedToServer = null;
+                OnDataReceivedFromServer = null;
+            }
+
+            public bool RunClient(string stringIPAddress, uint port = 12345)
+            {
+                if (IsClientReady)
+                {
+                    return false;
+                }
+
+                this.port = port;
+
+                IPAddress iPAddress = System.Net.IPAddress.Parse(stringIPAddress);
+                IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, (int)this.port);
+
+                tcpClient = new TcpClient();
+
+                IsClientReady = true;
+
+                if (OnClientStart != null)
+                {
+                    OnClientStart();
+                }
+
+                tcpClient.BeginConnect(iPEndPoint.Address.ToString(), iPEndPoint.Port,
+                    new AsyncCallback(BeginConnectCallback), tcpClient);
+
+                return true;
+            }
+
+            private void BeginConnectCallback(IAsyncResult asyncResult)
+            {
+                TcpClient tcpClient = (TcpClient)asyncResult.AsyncState;
+
+                tcpClient.EndConnect(asyncResult);
+                networkStream = tcpClient.GetStream();
+
+                readBuffer = new byte[readBufferSize];
+
+                networkStream.BeginRead(readBuffer, 0, (int)readBufferSize,
+                    new AsyncCallback(ClientIDReceiveCallback), null);
+            }
+
+            private void ClientIDReceiveCallback(IAsyncResult asyncResult)
+            {
+                int readBytesCount = networkStream.EndRead(asyncResult);
+
+                using (ReadablePacket readablePacket = PacketPacker.PackIncomingPacket(readBytesCount, readBuffer))
+                {
+                    if (readablePacket.PacketType != PacketType.SYSTEM || readablePacket.SourceClientID != Constant.SERVER_ID)
+                    {
+                        return;
+                    }
+
+                    ServerPacketInformation information = (ServerPacketInformation)readablePacket.ReadInt();
+
+                    if (information == ServerPacketInformation.SERVER_CLIENT_ID_SEND)
+                    {
+                        ClientID = readablePacket.ReadUInt();
+
+                        using (WritablePacket writablePacket = new WritablePacket(PacketType.SYSTEM))
+                        {
+                            writablePacket.Write((int)ClientPacketInformation.CLIENT_RECEIVED_ID_WELL);
+
+                            byte[] data = PacketPacker.PackOutgoingPacket(ClientID, writablePacket);
+
+                            networkStream.BeginWrite(data, 0, data.Length, null, null);
+                        }
+
+                        IsClientReadable = true;
+                        IsClientWritable = true;
+
+                        Read();
+
+                        if (OnConnectedToServer != null)
+                        {
+                            OnConnectedToServer();
+                        }
+                    }
+
+                    else
+                    {
+                        networkStream.BeginRead(readBuffer, 0, (int)readBufferSize,
+                        new AsyncCallback(ClientIDReceiveCallback), null);
+                    }
+                }
+            }
+
+            public void Send(byte[] dataToSend)
+            {
+                if (!IsClientReady)
+                {
+                    return;
+                }
+
+                Send(dataToSend, 0, dataToSend.Length);
+            }
+
+            private void Send(byte[] dataToSend, int offset, int count)
+            {
+                networkStream.BeginWrite(dataToSend, offset, count, null, null);
+            }
+
+            public void Read()
+            {
+                if (!IsClientReady)
+                {
+                    return;
+                }
+
+                Read(0, (int)readBufferSize);
+            }
+
+            private void Read(int offset, int count)
+            {
+                Array.Clear(readBuffer, 0, (int)readBufferSize);
+
+                networkStream.BeginRead(readBuffer, offset, count, new AsyncCallback(BeginReadCallback), networkStream);
+            }
+
+            private void BeginReadCallback(IAsyncResult asyncResult)
+            {
+                NetworkStream networkStream = (NetworkStream)asyncResult.AsyncState;
+
+                if (networkStream == null || !IsClientReady)
+                {
+                    return;
+                }
+
+                int readBytesCount = networkStream.EndRead(asyncResult);
+
+                ReadablePacket readablePacket = PacketPacker.PackIncomingPacket(readBytesCount, readBuffer);
+
+                if (readablePacket.SourceClientID != Server.SERVER_ID)
+                {
+                    return;
+                }
+
+                if (readablePacket.PacketType == PacketType.SYSTEM)
+                {
+                    ServerPacketInformation serverPacketInformation = (ServerPacketInformation)readablePacket.ReadInt();
+
+                    if (serverPacketInformation == ServerPacketInformation.SERVER_HAS_STOPPED)
+                    {
+                        StopClient(true);
+                        return;
+                    }
+                }
+
+                if (readablePacket.PacketType == PacketType.CUSTOM || OnDataReceivedFromServer != null)
+                {
+                    OnDataReceivedFromServer(readBytesCount, PacketPacker.PackIncomingPacket(readBytesCount, readBuffer));
+                }
+
+                Array.Clear(readBuffer, 0, (int)readBufferSize);
+
+                networkStream.BeginRead(readBuffer, 0, (int)readBufferSize, new AsyncCallback(BeginReadCallback), networkStream);
+            }
+
+            public bool StopClient(bool isForced = false)
+            {
+                if (!IsClientReady)
+                {
+                    return false;
+                }
+
+                if (!isForced)
+                {
+                    using (WritablePacket writablePacket = new WritablePacket(PacketType.SYSTEM))
+                    {
+                        writablePacket.Write((int)ClientPacketInformation.CLIENT_DISCONNECT_REQUEST);
+
+                        networkStream.Write(PacketPacker.PackOutgoingPacket(ClientID, writablePacket));
+                    }
+                }
+
+                if (OnClientStop != null)
+                {
+                    OnClientStop();
+                }
+
+                networkStream?.Close();
+                tcpClient?.Close();
+
+                IsClientReadable = false;
+                IsClientWritable = false;
+                IsClientReady = false;
+
+                return true;
+            }
+        }
+    }
+}
---
+++ JustNet/WritablePacket.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using static JustNet.NetworkRunner.Constant;
+
+namespace JustNet
+{
+    public class WritablePacket : Packet
+    {
+        private List<byte> buffer;
+
+        public int Size { get => buffer.Count; }
+
+        internal WritablePacket(PacketType packetType, uint sourceClientID) : base(packetType, sourceClientID)
+        {
+            buffer = new List<byte>();
+        }
+
+        internal WritablePacket(PacketType packetType, uint sourceClientID, byte[] buffer) : base(packetType, sourceClientID)
+        {
+            this.buffer = new List<byte>();
+            this.buffer.AddRange(buffer);
+        }
+
+        public byte[] ToArray() => buffer.ToArray();
+
+        public void Insert(int index, byte[] data) => buffer.InsertRange(index, data);
+
+        public void Write(byte data)
+        {
+            buffer.Add(data);
+        }
+
+        public void Write(short data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(ushort data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(int data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(uint data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(long data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(ulong data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(float data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(double data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(bool data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(string data)
+        {
+            byte[] converted = Encoding.UTF8.GetBytes(data);
+            Write(converted.Length); // Write length of string data
+            buffer.AddRange(converted);
+        }
+
+        public void Write(char data)
+        {
+            buffer.AddRange(BitConverter.GetBytes(data));
+        }
+
+        public void Write(INetworkData networkData)
+        {
+            networkData.WriteData(this);
+        }
+
+        protected override void DisposeFunction()
+        {
+            buffer = null;
+        }
+    }
+}
